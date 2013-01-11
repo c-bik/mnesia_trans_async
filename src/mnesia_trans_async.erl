c7a8d4d87d507ffba3e013dba042217ba55dfcd6
Bikram Chatterjee <razorpeak@gmail.com>
2013-01-11 10:48:57 +0100

    insert by seperate process in parallel with transaction

--- src/mnesia_trans_async.erl
+++ src/mnesia_trans_async.erl
@@ -7,26 +7,39 @@
 %% Application callbacks
 %% ===================================================================
 
-setup(RowCount) ->
+setup() ->
     {atomic, ok} = mnesia:create_table(table, [{attributes, [col1, col2, col3]}]),
-    _ = [mnesia:dirty_write({table, R, R+1, R+2}) || R <- lists:seq(1,RowCount)],
     ok.
 
 -define(ROWCOUNT, 100).
--define(THREAD_A_DELAY, 100).
--define(THREAD_B_DELAY, 200).
+-define(THREAD_A_DELAY, 2000).
+-define(THREAD_B_DELAY, 2000).
 -define(THREAD_A_CHUNK, 10).
 -define(THREAD_B_CHUNK, 20).
 
 run_test() ->
     mnesia:stop(),
     mnesia:start(),
-    setup(?ROWCOUNT),
+    setup(),
+    async_insert(),
+    timer:sleep(?THREAD_A_DELAY),
     recv_async("_A_", ?THREAD_A_CHUNK, ?THREAD_A_DELAY),
     recv_async("_B_", ?THREAD_B_CHUNK, ?THREAD_B_DELAY),
     TotalDelay = round(?ROWCOUNT / ?THREAD_A_CHUNK * ?THREAD_A_DELAY + ?ROWCOUNT / ?THREAD_B_CHUNK * ?THREAD_B_DELAY),
     io:format("waiting... ~p~n", [TotalDelay]),
     timer:sleep(TotalDelay).
+
+async_insert() ->
+    F = fun
+            (_, 0) -> ok;
+            (F, R) ->
+                mnesia:transaction(fun() ->
+                    mnesia:write({table, R, R+1, R+2})
+                end),
+                timer:sleep(?THREAD_B_DELAY div 20),
+                F(F,R-1)
+    end,
+    spawn(fun() -> F(F, ?ROWCOUNT) end).
 
 recv_async(Title, Limit, Delay) ->
     F0 = fun() ->
@@ -35,7 +48,7 @@ recv_async(Title, Limit, Delay) ->
             Pid ! next,
             receive
                 eot ->
-                    io:format("finished~n", []);
+                    io:format("[~p] finished~n", [Title]);
                 {row, Row} ->
                     io:format("[~p] got rows ~p~n", [Title, length(Row)]),
                     timer:sleep(Delay),
