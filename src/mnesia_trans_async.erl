9a1d486461a49bdf513add6fe33f1be5ccfccd65
Bikram Chatterjee <razorpeak@gmail.com>
2013-01-14 11:26:06 +0100

    tests run through eunit

--- src/mnesia_trans_async.erl
+++ src/mnesia_trans_async.erl
@@ -3,30 +3,54 @@
 %% Application callbacks
 -compile(export_all).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
-setup() ->
-    {atomic, ok} = mnesia:create_table(table, [{attributes, [col1, col2, col3]}]),
-    ok.
+-include_lib("eunit/include/eunit.hrl").
 
 -define(ROWCOUNT, 100).
 -define(THREAD_A_DELAY, 2000).
 -define(THREAD_B_DELAY, 2000).
 -define(THREAD_A_CHUNK, 10).
 -define(THREAD_B_CHUNK, 20).
+-define(TEST_TIMEOUT, 1000000).
 
-run_test() ->
-    mnesia:stop(),
+% EUnit tests --
+setup() ->
     mnesia:start(),
-    setup(),
+    {atomic, ok} = mnesia:create_table(table, [{attributes, [col1, col2, col3]}]),
+    ok.
+
+teardown(_) ->
+    mnesia:delete_table(table),
+    mnesia:stop().
+
+imem_mnesia_test_() ->
+    {timeout, ?TEST_TIMEOUT, {
+        setup,
+        fun setup/0,
+        fun teardown/1,
+        {with, [
+                fun run_test_eunit/1
+        ]}
+        }
+    }.
+
+run_test_eunit(_) ->
+    io:format(user, "EUNIT ---~n", []),
+    run_test_core().
+
+run_console() ->
+    io:format(user, "CONSOLE ---~n", []),
+    mnesia:start(),
+    {atomic, ok} = mnesia:create_table(table, [{attributes, [col1, col2, col3]}]),
+    run_test_core(),
+    mnesia:delete_table(table),
+    mnesia:stop().
+
+run_test_core() ->
     async_insert(),
-    timer:sleep(?THREAD_A_DELAY),
     recv_async("_A_", ?THREAD_A_CHUNK, ?THREAD_A_DELAY),
     recv_async("_B_", ?THREAD_B_CHUNK, ?THREAD_B_DELAY),
     TotalDelay = round(?ROWCOUNT / ?THREAD_A_CHUNK * ?THREAD_A_DELAY + ?ROWCOUNT / ?THREAD_B_CHUNK * ?THREAD_B_DELAY),
-    io:format("waiting... ~p~n", [TotalDelay]),
+    io:format(user, "waiting... ~p~n", [TotalDelay]),
     timer:sleep(TotalDelay).
 
 async_insert() ->
@@ -34,24 +58,25 @@ async_insert() ->
             (_, 0) -> ok;
             (F, R) ->
                 mnesia:transaction(fun() ->
+                    io:format(user, "insert ~p~n", [R]),
                     mnesia:write({table, R, R+1, R+2})
                 end),
                 timer:sleep(?THREAD_B_DELAY div 20),
                 F(F,R-1)
     end,
-    spawn(fun() -> F(F, ?ROWCOUNT) end).
+    spawn(fun() -> F(F, ?ROWCOUNT div 3) end).
 
 recv_async(Title, Limit, Delay) ->
     F0 = fun() ->
-        Pid = start_trans(self(), Limit),
+        Pid = start_trans(self(), Title, Limit),
         F = fun(F) ->
+            timer:sleep(Delay),
             Pid ! next,
             receive
                 eot ->
-                    io:format("[~p] finished~n", [Title]);
+                    io:format(user, "[~p] finished~n", [Title]);
                 {row, Row} ->
-                    io:format("[~p] got rows ~p~n", [Title, length(Row)]),
-                    timer:sleep(Delay),
+                    io:format(user, "[~p] got rows ~p~n", [Title, length(Row)]),
                     F(F)
             end
         end,
@@ -59,22 +84,23 @@ recv_async(Title, Limit, Delay) ->
     end,
     spawn(F0).
 
-start_trans(Pid, Limit) ->
+start_trans(Pid, Title, Limit) ->
     F =
     fun(F,Contd0) ->
         receive
             abort ->
-                io:format("Abort~n", []);
+                io:format(user, "~p Abort~n", [Title]);
             next ->
                 case (case Contd0 of
                       undefined -> mnesia:select(table, [{'$1', [], ['$_']}], Limit, read);
                       Contd0 -> mnesia:select(Contd0)
                       end) of
                 {Rows, Contd1} ->
+                    io:format(user, "[~p] next~n", [Title]),
                     Pid ! {row, Rows},
                     F(F,Contd1);
                 '$end_of_table' -> Pid ! eot
                 end
         end
     end,
-    spawn(mnesia, transaction, [F, [F,undefined]]).
+    spawn(mnesia, async_dirty, [F, [F,undefined]]).
